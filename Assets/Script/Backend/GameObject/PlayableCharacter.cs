6a947f252f99c6c49fd45ba89e29a0b83aff111f
Jialuo Gao <jialuogao980513@gmail.com>
2023-06-04 00:15:48 -0700

    Add backend general implementation v1

---
+++ Assets/Script/Backend/GameObject/PlayableCharacter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assets.Script.Backend
+{
+    public class PlayableCharacter: HittableObject
+    {
+        public PlayableCharacter(string name, PlayableCharacterStats characterStats) 
+            : base(name, characterStats, HittableObjectType.PlayableCharacter)
+        {
+        }
+
+        public void GotHit(int incomingDmg)
+        {
+            this.HittableObjectStats.GotHit(incomingDmg);
+            if (!this.HittableObjectStats.IsAlive)
+            {
+                EventManager.TriggerEvent("PlayerDead");
+            }
+        }
+    }
+
+    public class PlayableCharacterStats: HittableObjectStats
+    {
+        public int Level { get; set; }
+
+        public int CurrentExp = 0;
+
+        public const int LevelUpExp = 100;
+
+        public PlayableCharacterStats(int maxHitPoint, int attackDmg, int defense, int level = 1, int currentExp = 0) : base(maxHitPoint, attackDmg, defense)
+        {
+            Level = level;
+            CurrentExp = currentExp;
+        }
+
+        public virtual void GotExp(int Exp)
+        {
+            Interlocked.Add(ref CurrentExp, Exp);
+            Level = CurrentExp / LevelUpExp + 1;
+        }
+    }
+}
---
+++ Assets/Script/Backend/GameStages/GameStageBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Assets.Script.Backend
+{
+    public class GameStageBase
+    {
+        protected ConcurrentDictionary<string, HittableObject> HittableObjectCollection { get; set; }
+
+        public GameStageBase()
+        {
+            HittableObjectCollection = new ConcurrentDictionary<string, HittableObject>();
+        }
+
+        public void InitializeStage()
+        {
+            InitializeCharacters();
+        }
+
+        protected void AddHittableObject(string objectName, HittableObject hittableObjects)
+        {
+            if (HittableObjectCollection.ContainsKey(objectName))
+            {
+                throw new Exception($"Object with name: {objectName} already exist in current stage");
+            }
+            HittableObjectCollection[objectName] = hittableObjects;
+        }
+
+        protected virtual HittableObjectStats LoadEnemyStats()
+        {
+            return new HittableObjectStats(maxHitPoint: 100, attackDmg: 30, defense: 10);
+        }
+
+        protected virtual PlayableCharacterStats LoadCharacterStats()
+        {
+            return new PlayableCharacterStats(maxHitPoint: 100, attackDmg: 30, defense: 10);
+        }
+
+        public virtual void InitializeCharacters()
+        {
+            AddHittableObject("Player", new PlayableCharacter("Player", LoadCharacterStats()));
+        }
+
+        public virtual void ProcessDamage(DamangeSource srcObjectName, DamageTarget targetObjectName)
+        {
+            var targetObj = HittableObjectCollection[targetObjectName.TgtObjectName];
+            targetObj.GotHit(CalculateDamage(srcObjectName, targetObjectName));
+
+            if (!targetObj.IsAlive)
+            {
+                HittableObjectCollection.Remove(targetObj.Name, out _);
+            }
+        }
+
+        public virtual int CalculateDamage(DamangeSource srcObjectName, DamageTarget targetObjectName)
+        {
+            return 100;
+        }
+    }
+}
