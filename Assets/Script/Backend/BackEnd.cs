0c4bced918115480cb1e778bd67d75a0ec69adb9
yinuo <ywang989@outlook.com>
2023-06-10 23:35:50 -0400

    Enemy Lock: create backend script

--- Assets/Script/Backend/BackEnd.cs
+++ Assets/Script/Backend/BackEnd.cs
@@ -7,13 +7,13 @@ using UnityEngine;
 
 namespace Assets.Script.Backend
 {
-    public class main : MonoBehaviour
+    public class BackEnd : MonoBehaviour
     {
         public static GameStageBase GameLoop;
 
         public void Start()
         {
-            GameLoop = new GameStageBase();
+            GameLoop = new GameStageTest();
             GameLoop.InitializeStage();
         }
     }
