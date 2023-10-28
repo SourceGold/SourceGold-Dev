9affe4d2e22ff33a67cdc26db364607e4b9ae20e
Yida Zhang <zyd200101@outlook.com>
2023-10-27 21:31:07 -0400

    new camera

--- Assets/Script/Camera/CameraManager.cs
+++ Assets/Script/Camera/CameraManager.cs
@@ -12,7 +12,17 @@ public class CameraManager : MonoBehaviour
     public Transform NearestLockOnTarget;
     public GameObject CinemachineCameraTarget;
     public CinemachineVirtualCamera AimCamera;
+    public CinemachineVirtualCamera LockCamera;
     public Transform Crosshair;
+
+    private enum CameraState
+    {
+        Follow,
+        Aim,
+        LockOn,
+    }
+
+    private CameraState currentState = CameraState.Follow;
 
     [Tooltip("How far in degrees can you move the camera up")]
     public float TopClamp = 70.0f;
@@ -24,7 +34,7 @@ public class CameraManager : MonoBehaviour
 
     List<CharacterManager> availableTargets = new List<CharacterManager>();
 
-    private Animator _anim;
+    //private Animator _anim;
 
     private float _cinemachineTargetYaw;
     private float _cinemachineTargetPitch;
@@ -34,12 +44,13 @@ public class CameraManager : MonoBehaviour
     private void Awake()
     {
         AimCamera.gameObject.SetActive(false);
+        LockCamera.gameObject.SetActive(false);
     }
 
     private void Start()
     {
         _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
-        _anim = GetComponentInChildren<Animator>();
+        //_anim = GetComponentInChildren<Animator>();
     }
 
     private void LateUpdate()
@@ -66,27 +77,48 @@ public class CameraManager : MonoBehaviour
         _input = context.ReadValue<Vector2>();
     }
 
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawSphere(transform.position, 1);
+    //}
+
     private void CameraRotation()
     {
-        //if there is an input and camera position is not fixed
-        if (_input.sqrMagnitude >= _threshold)
+        if (currentState == CameraState.LockOn)
         {
-            //Don't multiply mouse input by Time.deltaTime;
-            //float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
-            float deltaTimeMultiplier = 1.0f;
+            Vector3 dir = NearestLockOnTarget.position - transform.position;
+            dir.Normalize();
+            dir.y = 0;
 
-            _cinemachineTargetYaw += _input.x * deltaTimeMultiplier;
-            _cinemachineTargetPitch += _input.y * deltaTimeMultiplier;
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            targetRotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 10);
+            CinemachineCameraTarget.transform.rotation = targetRotation;
+            _cinemachineTargetYaw = ClampAngle(targetRotation.eulerAngles.y, float.MinValue, float.MaxValue);
+            _cinemachineTargetPitch = ClampAngle(targetRotation.eulerAngles.x, BottomClamp, TopClamp);
         }
+        else
+        {
 
-        // clamp our rotations so our values are limited 360 degrees
-        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+            //if there is an input and camera position is not fixed
+            if (_input.sqrMagnitude >= _threshold)
+            {
+                //Don't multiply mouse input by Time.deltaTime;
+                //float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+                float deltaTimeMultiplier = 1.0f;
 
-        // Cinemachine will follow this target
-        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch, _cinemachineTargetYaw, 0.0f);
-        //CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
-        //    _cinemachineTargetYaw, 0.0f);
+                _cinemachineTargetYaw += _input.x * deltaTimeMultiplier;
+                _cinemachineTargetPitch += _input.y * deltaTimeMultiplier;
+            }
+
+            // clamp our rotations so our values are limited 360 degrees
+            _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+            // Cinemachine will follow this target
+            CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch, _cinemachineTargetYaw, 0.0f);
+            //CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+            //    _cinemachineTargetYaw, 0.0f);
+        }
     }
 
     private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
@@ -99,48 +131,50 @@ public class CameraManager : MonoBehaviour
     public Transform HandleLockOn()
     {
         float shortestDistance = Mathf.Infinity;
-        Collider[] colliders = Physics.OverlapSphere(TargetTransform.position, 26);
+        currentState = CameraState.LockOn;
+        LockCamera.gameObject.SetActive(true);
+        //Collider[] colliders = Physics.OverlapSphere(TargetTransform.position, 26);
 
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            CharacterManager character = colliders[i].GetComponent<CharacterManager>();
+        //for (int i = 0; i < colliders.Length; i++)
+        //{
+        //    CharacterManager character = colliders[i].GetComponent<CharacterManager>();
 
-            if (character != null)
-            {
-                Vector3 lockTargetDirection = character.transform.position - TargetTransform.position;
-                float distanceFromTarget = Vector3.Distance(TargetTransform.position, character.transform.position);
-                float viewableAngle = Vector3.Angle(lockTargetDirection, CameraTransform.forward);
+        //    if (character != null)
+        //    {
+        //        Vector3 lockTargetDirection = character.transform.position - TargetTransform.position;
+        //        float distanceFromTarget = Vector3.Distance(TargetTransform.position, character.transform.position);
+        //        float viewableAngle = Vector3.Angle(lockTargetDirection, CameraTransform.forward);
 
-                if (character.transform.root != TargetTransform.transform.root
-                    && viewableAngle > -50 && viewableAngle < 50
-                    && distanceFromTarget <= MaxLockOnDistance)
-                {
-                    availableTargets.Add(character);
-                }
-            }
+        //        if (character.transform.root != TargetTransform.transform.root
+        //            && viewableAngle > -50 && viewableAngle < 50
+        //            && distanceFromTarget <= MaxLockOnDistance)
+        //        {
+        //            availableTargets.Add(character);
+        //        }
+        //    }
 
-        }
+        //}
 
-        for (int i = 0; i < availableTargets.Count; i++)
-        {
-            float distanceFromTarget = Vector3.Distance(TargetTransform.position, availableTargets[i].lockOnTransForm.position);
-            if (distanceFromTarget < shortestDistance)
-            {
-                shortestDistance = distanceFromTarget;
-                NearestLockOnTarget = availableTargets[i].transform;
-            }
-        }
+        //for (int i = 0; i < availableTargets.Count; i++)
+        //{
+        //    float distanceFromTarget = Vector3.Distance(TargetTransform.position, availableTargets[i].lockOnTransForm.position);
+        //    if (distanceFromTarget < shortestDistance)
+        //    {
+        //        shortestDistance = distanceFromTarget;
+        //        NearestLockOnTarget = availableTargets[i].transform;
+        //    }
+        //}
 
-        if (NearestLockOnTarget)
-            _anim.SetBool("lock", true);
 
         return NearestLockOnTarget;
     }
 
-    public void ClearLockOnTargets()
+    public void StopLockOn()
     {
-        availableTargets.Clear();
-        NearestLockOnTarget = null;
-        _anim.SetBool("lock", false);
+        currentState = CameraState.Follow;
+        //availableTargets.Clear();
+        //NearestLockOnTarget = null;
+        //_anim.SetBool("lock", false);
+        LockCamera.gameObject.SetActive(false);
     }
 }
