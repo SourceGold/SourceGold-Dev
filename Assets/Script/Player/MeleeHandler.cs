8394b342081f33feeaef6ebe05405a0afcb4158e
AlstonXiao <alston_xiao@outlook.com>
2023-11-04 20:30:36 -0700

    move register to start to avoid race condition

--- Assets/Script/Player/MeleeHandler.cs
+++ Assets/Script/Player/MeleeHandler.cs
@@ -31,21 +31,22 @@ public class MeleeHandler : MonoBehaviour
 
     private void Awake()
     {
-        input = FindObjectOfType<ControlManager>().InputMap;
 
-        input.Player.MeleeAttack1.performed         += StandingMeleeAttack1;
-        input.Player.MeleeAttack2Press.performed    += StandingMeleeAttack2Press;
-        input.Player.EquipWeapon.performed          += EquipWeapon;
-        input.Player.SkillMeleeAttack1.performed    += SkillMeleeAttack1;
-        input.Player.SwitchWeapon.performed         += SwitchWeapon;
-        input.Player.MeleeAttack2Release.performed  += StandingMeleeAttack2Release;
-        input.Player.MeleeAttack3.performed         += StandingMeleeAttack3;
     }
 
 
     void Start()
     {
         _anim = GetComponent<Animator>();
+        input = FindObjectOfType<ControlManager>().InputMap;
+
+        input.Player.MeleeAttack1.performed += StandingMeleeAttack1;
+        input.Player.MeleeAttack2Press.performed += StandingMeleeAttack2Press;
+        input.Player.EquipWeapon.performed += EquipWeapon;
+        input.Player.SkillMeleeAttack1.performed += SkillMeleeAttack1;
+        input.Player.SwitchWeapon.performed += SwitchWeapon;
+        input.Player.MeleeAttack2Release.performed += StandingMeleeAttack2Release;
+        input.Player.MeleeAttack3.performed += StandingMeleeAttack3;
     }
 
     void Update()
