6e926402299cd2de82b511938837954672b2500d
Yida Zhang <zyd200101@outlook.com>
2024-03-04 00:37:23 -0500

    fix aim rotation

--- Assets/Script/Enemy/SimpleRanged.cs
+++ Assets/Script/Enemy/SimpleRanged.cs
@@ -1,9 +1,11 @@
+using Assets.Script.Backend;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class SimpleRanged : MonoBehaviour
 {
@@ -12,6 +14,7 @@ public class SimpleRanged : MonoBehaviour
     public Transform SpawnBulletPosition;
     public float DetectionAngle = 30f;
     public float DetectionRadius = 15f;
+    [Tooltip("Degree per second")]
     public float AimRotationSpeed = 5f;
     public float ReleaseLockTime = 0.5f;
     public float BulletSpeed = 200f;
@@ -24,10 +27,14 @@ public class SimpleRanged : MonoBehaviour
     private LayerMask PlayerMask;
     private Transform LockedTarget;
     private float NoTargetTime;
+    private float BulletPositionHeight;
+    private Transform AlwaysUp;
+    private Vector3 CurrentRotateionVelocity;
 
     private void Awake()
     {
         pfBulletProjectile = Resources.Load("Prefab/Player/pfSimpleEnemyBulletProjectile");
+        AlwaysUp = transform.Find("Always Up");
     }
 
     // Start is called before the first frame update
@@ -35,6 +42,7 @@ public class SimpleRanged : MonoBehaviour
     {
         ShootWait += ShootDelay;
         PlayerMask = LayerMask.GetMask("Player");
+        BulletPositionHeight = SpawnBulletPosition.transform.position.y - transform.position.y;
     }
 
     // Update is called once per frame
@@ -58,6 +66,23 @@ public class SimpleRanged : MonoBehaviour
         return Physics.Raycast(Head.position, target.position - Head.position, DetectionRadius, PlayerMask);
     }
 
+    // https://math.stackexchange.com/questions/543496/how-to-find-the-equation-of-a-line-tangent-to-a-circle-that-passes-through-a-g
+    float Calculate(Transform target)
+    {
+        // transform target position to coordinate relative to self position;
+        Vector3 targetRelative = AlwaysUp.InverseTransformPoint(target.position);
+        // align x,z to x axis only
+        Vector2 targetPosition = new Vector2(Mathf.Sqrt(targetRelative.x * targetRelative.x + targetRelative.z * targetRelative.z), targetRelative.y);
+        Vector2 thisPosition = Vector2.zero;
+        float distance = Vector2.Distance(thisPosition, targetPosition);
+        Vector2 vector = targetPosition - thisPosition;
+        float radius = BulletPositionHeight;
+        float pho = radius / distance;
+        float pho2 = pho * pho;
+        Vector2 result = thisPosition + (pho2 * vector) + (pho * Mathf.Sqrt(1 - pho2) * Vector2.Perpendicular(vector));
+        return Vector2.Angle(new Vector2(0, radius), result - thisPosition);
+    }
+
     void Aim()
     {
         float shortestDistance = Mathf.Infinity;
@@ -66,7 +91,7 @@ public class SimpleRanged : MonoBehaviour
         foreach (var c in colliders)
         {
             Vector3 lockTargetDirection = c.transform.position - Head.position;
-            float distanceFromTarget = Vector3.Distance(Head.position, c.transform.position);
+            float distanceFromTarget = Vector3.Distance(transform.position, c.transform.position);
             float targetAngle = Vector3.Angle(lockTargetDirection, transform.forward);
             if (/*InSight(c.transform) &&*/ shortestDistance > distanceFromTarget && targetAngle < DetectionAngle && targetAngle > -DetectionAngle)
             {
@@ -83,7 +108,28 @@ public class SimpleRanged : MonoBehaviour
         }
 
         if (LockedTarget)
-            transform.rotation = Quaternion.Lerp(transform.rotation, 
-                Quaternion.LookRotation(LockedTarget.position - SpawnBulletPosition.position), AimRotationSpeed * Time.deltaTime);
+        {
+            Vector3 euler = Quaternion.LookRotation(LockedTarget.position - transform.position).eulerAngles;
+            if (shortestDistance > BulletPositionHeight)
+            {
+                float angle = Calculate(LockedTarget);
+                euler.x = angle;
+                float time = Mathf.Abs(Vector3.Angle(transform.eulerAngles, euler) / AimRotationSpeed);
+                transform.eulerAngles = Vector3.SmoothDamp(transform.eulerAngles, euler, ref CurrentRotateionVelocity, time);
+                euler = transform.eulerAngles;
+                euler.x = 0;
+                AlwaysUp.eulerAngles = euler;
+            }
+            else
+            {
+                euler.x = 80;
+                transform.eulerAngles = euler;
+                euler.x = 0;
+                AlwaysUp.eulerAngles = euler;
+                for (int i = 0; i < 50; i++)
+                    Backend.GameLoop.ProcessDamage(new DamangeSource() { SrcObjectName = transform.name },
+                        new DamageTarget() { TgtObjectName = "Player" });
+            }
+        }
     }
 }
