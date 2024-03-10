46485f650bb1498afd24a4c580cfeb0597658897
jialuogao <jialuogao980513@gmail.com>
2024-03-09 18:50:13 -0800

    Update Backend.cs

--- Assets/Script/Backend/Backend.cs
+++ Assets/Script/Backend/Backend.cs
@@ -32,7 +32,7 @@
         // test only
         public void OnApplicationQuit()
         {
-            DataPersistenceManager.SaveGame(DataPersistenceManager._testSave);
+            //DataPersistenceManager.SaveGame(DataPersistenceManager._testSave);
         }
 
         private GameSceneBase LoadStage(string stageName)
