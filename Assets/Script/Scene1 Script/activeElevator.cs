7986a1a7c0be6c607bcdc57b29dba0b0f75f432e
Yan Xiao <32349680+AlstonXiao@users.noreply.github.com>
2024-05-25 21:20:44 -0700

    adding animation (#71)
    Adding buttons in the scene to interact with other objects in the scene
    Adding conditional buttons on the inventory
    Adding animations to the non-player elements
    Modifying the displayed text.

---
+++ Assets/Script/Scene1 Script/activeElevator.cs
@@ -0,0 +1,57 @@
+using Assets.Script.Backend;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activeElevator : InteractableObject
+{
+    public float fullVisibleRange = -1f;
+    public float initialVisibleRange = -1f;
+
+    public Animator animator1;
+    public Animator animator2;
+    public Animator animator3;
+    public Animator animator4;
+    public Animator animator5;
+    public Animator animator6;
+    public Animator animator7;
+    public Animator animator8;
+    private SceneItemActiveUI uiElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiElement = GetComponentInChildren<SceneItemActiveUI>();
+        uiElement.setText("Press [F] to start");
+    }
+
+    public override void closestActivation()
+    {
+        if (uiElement != null)
+        {
+            uiElement.setActive(initialVisibleRange, fullVisibleRange);
+        }
+    }
+
+    public override void closestDeactivation()
+    {
+        if (uiElement != null)
+        {
+            uiElement.setInactive();
+        }
+    }
+    public override void playerInteract()
+    {
+        animator1.SetBool("TurnOnElevator", true);
+        animator2.SetBool("TurnOnElevator", true);
+        animator3.SetBool("TurnOnElevator", true);
+        animator4.SetBool("TurnOnElevator", true);
+        animator5.SetBool("TurnOnElevator", true);
+        animator6.SetBool("TurnOnElevator", true);
+        animator7.SetBool("TurnOnElevator", true);
+        animator8.SetBool("TurnOnElevator", true);
+    }
+
+    public override void inRange() { }
+    public override void outRange() { }
+}
---
+++ Assets/Script/Scene1 Script/doorOpen.cs
@@ -0,0 +1,55 @@
+using Assets.Script.Backend;
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : InteractableObject
+{
+    public float fullVisibleRange = -1f;
+    public float initialVisibleRange = -1f;
+
+    public Animator animator1;
+    private SceneItemActiveUI uiElement;
+    private Inventory playerInventory;
+    private GameItemDynamic test_item = new GameItemDynamic("Potion", dummy: true);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiElement = GetComponentInChildren<SceneItemActiveUI>();
+        uiElement.setText("You have to have 10 health potion. \nPress [F] to start");
+        playerInventory = Backend.GameLoop.PlayerInventory;
+    }
+
+    public override void closestActivation()
+    {
+        if (uiElement != null)
+        {
+            if (playerInventory.checkItem(test_item, 10))
+                uiElement.setText("Press [F] to use 10 potion to open door");
+            else
+                uiElement.setText("You don't have enough potion\nRequired 10");
+            uiElement.setActive(initialVisibleRange, fullVisibleRange);
+        }
+    }
+
+    public override void closestDeactivation()
+    {
+        if (uiElement != null)
+        {
+            uiElement.setInactive();
+        }
+    }
+    public override void playerInteract()
+    {
+        if (playerInventory.checkItem(test_item, 10))
+        {
+            playerInventory.RemoveItem(test_item, 10);
+            animator1.SetBool("OpenDoor", true);
+        }
+    }
+
+    public override void inRange() { }
+    public override void outRange() { }
+}
