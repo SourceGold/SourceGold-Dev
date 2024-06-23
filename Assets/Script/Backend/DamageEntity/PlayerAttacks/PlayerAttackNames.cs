c8b06dcf9c74be7b95886fa5a08f1cb6ec7dae93
Jialuo Gao <jialuogao980513@gmail.com>
2024-06-22 19:48:05 -0700

    add jump

--- Assets/Script/Backend/DamageEntity/PlayerAttacks/PlayerAttackNames.cs
+++ Assets/Script/Backend/DamageEntity/PlayerAttacks/PlayerAttackNames.cs
@@ -8,6 +8,7 @@
         public static readonly string SwordHeavy1Charged = "Sword.Heavy.1.Charged";
         public static readonly string SwordHeavy2Uncharged = "Sword.Heavy.2.Uncharged";
         public static readonly string SwordHeavy2Charged = "Sword.Heavy.2.Charged";
+        public static readonly string SwordJump = "Sword.Jump";
         public static readonly string KatanaLight1 = "Katana.Light.1";
         public static readonly string KatanaLight2 = "Katana.Light.2";
         public static readonly string KatanaLight3 = "Katana.Light.3";
@@ -15,5 +16,6 @@
         public static readonly string KatanaHeavy1Charged = "Katana.Heavy.1.Charged";
         public static readonly string KatanaHeavy2Uncharged = "Katana.Heavy.2.Uncharged";
         public static readonly string KatanaHeavy2Charged = "Katana.Heavy.2.Charged";
+        public static readonly string KatanaJump = "Katana.Jump";
     }
 }
